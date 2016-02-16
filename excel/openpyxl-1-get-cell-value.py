180fccc64f88a2ea88370fdcdfcb82b7e8921ebb
root <root@C6X-52.loc>
2016-02-17 05:00:05 +0800

    remove exceldir

--- excel/openpyxl-1-get-cell-value.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-from openpyxl import Workbook
-from openpyxl import load_workbook
-
-wb = load_workbook(filename = 'test.xlsx')
-ws1 = wb.worksheets[0]
-
-c1 = ws1.cell(row=1,column=1).value
-print c1
