6ff8e8dda0160469c573a7968744cee62bf978ea
root <root@C6X-52.loc>
2016-02-19 02:01:30 +0800

    add datetime-2

---
+++ infra/datetime-2-ntp-checkdatetime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import datetime
+import ntplib
+client = ntplib.NTPClient()
+r_xkl = client.request('192.168.1.232')
+r_win = client.request('time.windows.com')
+
+time1 = datetime.datetime.fromtimestamp(r_xkl.tx_time)
+time2 = datetime.datetime.fromtimestamp(r_win.tx_time)
+
+loc1 = "xkl"
+loc2 = "windows.com"
+
+time_text = "{location} time is : {time}." 
+
+print time_text.format(location=loc1,time=time1)
+print time_text.format(location=loc2,time=time2)
